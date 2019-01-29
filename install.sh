15bf285fb746ff9674753fd508d47d161f5f7d10
Ying Mao <giggle.my@gmail.com>
2019-01-29 13:08:22 -0500

    workers/manager

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ do
     else
         ssh root@$line -n "rm -rf /mapreduce-test/ && mkdir /mapreduce-test/"
         echo "Copy data to $line"
-        scp  /mapreduce-test/setup.py root@$line:/mapreduce-test/ && scp /mapreduce-test/master root@$line:/mapreduce-test/ && scp /mapreduce-test/slaves root@$line:/mapreduce-test/
+        scp  /mapreduce-test/setup.py root@$line:/mapreduce-test/ && scp /mapreduce-test/manager root@$line:/mapreduce-test/ && scp /mapreduce-test/workers root@$line:/mapreduce-test/
         echo "Setup $line"
         ssh root@$line -n "cd /mapreduce-test/ && python3 setup.py && ntpdate time.nist.gov"
         echo "Finished config node $line"
